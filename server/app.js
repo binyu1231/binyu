d3ea4656ed6d094dbf6a4b8bb6fdb590708f1c03
WangBinyu <406279919@qq.com>
2021-07-23 15:16:40 +0800

    update

--- server/app.js
+++ server/app.js
@@ -11,11 +11,12 @@ const whitelist = [
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
+    callback(null, true)
+    // if (whitelist.indexOf(origin) !== -1) {
+    //   callback(null, true)
+    // } else {
+    //   callback(new Error('Not allowed by CORS'))
+    // }
   }
 }
 
